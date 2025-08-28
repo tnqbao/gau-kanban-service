317223fa34ef740cf591d7b4b89b505f72576fe1
tnqbao <quocbao.job106204@gmail.com>
2025-08-28 08:36:02 +0700

    refactor: board create with creator fullname

--- controller/dto.go
+++ controller/dto.go
@@ -153,6 +153,7 @@ type ChangeTicketPositionRequest struct {
 type CreateBoardRequest struct {
 	Title       string `json:"title" binding:"required"`
 	Description string `json:"description"`
+	FullName    string `json:"fullname" binding:"required"`
 }
 
 type UpdateBoardRequest struct {
